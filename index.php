0c663332db2c3c1bbe3c9cfe0c797ed66f02445f
Simone Ronca <Simone Ronca@DESKTOP-SV92K8S.homenet.telecomitalia.it>
2018-03-14 11:02:38 +0100

    Primo commit

---
+++ index.php
@@ -0,0 +1,93 @@
+  <html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = $_REQUEST['fname'];
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name." is this";
+    }
+}	
+echo "<br>";
+echo "<br>";
+?>
+
+
+ 
+ 
+ 
+ <?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo count($cars);
+echo "<br>";
+
+
+$arrlength = count($cars);
+
+$cars[$arrlength] = "VW";
+
+for($x = 0; $x < count($cars); $x++) {
+    echo $cars[$x];
+    echo "<br>";
+}
+
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+$age["Simone"] = "42"; 
+
+foreach($age as $chiave => $valore) {
+    echo "Key=" . $chiave . ", Value=" . $valore;
+    echo "<br>";
+}
+
+echo "<br>";
+echo "Sort by value<br>";
+asort($age);
+foreach($age as $chiave => $valore) {
+    echo "Key=" . $chiave . ", Value=" . $valore;
+    echo "<br>";
+}
+
+echo "<br>";
+echo "Sort by key<br>";
+ksort($age);
+foreach($age as $chiave => $valore) {
+    echo "Key=" . $chiave . ", Value=" . $valore;
+    echo "<br>";
+}
+
+$x = 75;
+$y = 25;
+ 
+function addition() {
+	$x = 10;
+    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+}
+ 
+addition();
+echo $z;
+
+echo "<br>";
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+
+?>
+
+
+</body>
+</html> 
